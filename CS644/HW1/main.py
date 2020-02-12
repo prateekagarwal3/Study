8b0ae0d5e43ee8f1d1b362f915497ae4adf3dbde
prateekagarwal3 <pa59946@usc.edu>
2020-02-12 09:18:15 -0800

    seems to be working

--- CS644/HW1/main.py
+++ CS644/HW1/main.py
@@ -5,20 +5,20 @@ for res in f.readlines():
     res = res.split('\t')
     restaurants[res[0].lower()] = {"number":res[1].lower(), "foodtype":res[2].lower(), "price":res[3].lower(), "location":res[4].split('\n')[0].lower()}
 
-# print(restaurants)
+print(restaurants)
 foodTypes = ["chinese", "japanese", "mexican", "italian", "greek"]
 prices = ["cheap", "medium-priced", "expensive"]
 locations = ["marina del ray", "korea town", "venice", "playa vista", "santa monica", "hollywood"]
 
-def process_input(ip):
+def process_confirmation_ip(ip):
     ip = ip.lower()
-    any_found = False
-    ft = None
-    p = None
-    l = None
-
-    if ip.find("any") != -1:
-        any_found = True
+    if ip.find("yes") != -1:
+        return 1
+    else:
+        return 0
+    
+def process_ft_input(ip, ft, p, l):
+    ip = ip.lower()
 
     for i in foodTypes:
         if ip.find(i) != -1:
@@ -34,64 +34,158 @@ def process_input(ip):
         if ip.find(i) != -1:
             l = i
             break
-    return any_found, ft, p, l
+    if ft is None:
+        if ip.find('any') != -1:
+            ft = 'any'
+    return ft, p, l
 
-def process_confirmation_ip(ip):
+def process_p_input(ip, ft, p, l):
     ip = ip.lower()
-    if ip.find("yes") != -1:
-        return 1
-    else:
-        return 0
-    
-ft_confirm = False
-p_confirm = False
-l_confirm = False
 
-while(1):
-    if ft_confirm is False:
-        print("What type of restaurant do you want?")
-        ip = raw_input()
-        any_found, ft, p, l = process_input(ip)
+    for i in foodTypes:
+        if ip.find(i) != -1:
+            ft = i
+            break
 
-        if ft is None and any_found is False:
+    for i in prices:
+        if ip.find(i) != -1:
+            p = i
+            break
+
+    for i in locations:
+        if ip.find(i) != -1:
+            l = i
+            break
+        
+    if p is None:
+        if ip.find('any') != -1:
+            p = 'any'
+
+    return ft, p, l
+
+def process_l_input(ip, ft, p, l):
+    ip = ip.lower()
+
+    for i in foodTypes:
+        if ip.find(i) != -1:
+            ft = i
+            break
+
+    for i in prices:
+        if ip.find(i) != -1:
+            p = i
+            break
+
+    for i in locations:
+        if ip.find(i) != -1:
+            l = i
+            break
+        
+    if l is None:
+        if ip.find('any') != -1:
+            l = 'any'
+
+    return ft, p, l
+
+def get_foodtype(ft, p, l):
+    while(1):
+        if ft is None:
+            print("What type of restaurant do you want?")
+            ip = raw_input()
+            ft, p, l = process_ft_input(ip, ft, p, l)
+
+        if ft is None:
             continue
         else:
-            print("You said you want {} restaurant?".format(ft))
+            print("You said you want {} foodtype restaurant?".format(ft))
             ip = raw_input()
             ft_confirm = process_confirmation_ip(ip)
-            print("confirmation:", ft_confirm)
         
-        if ft_confirm is 0:
-            continue
-
-    if p_confirm is False:
-        print("How expensive a restaurant do you want?")
-        ip = raw_input()
-        any_found, ft, p, l = process_input(ip)
-
-        if p is None and any_found is False:
+        if ft_confirm == 0:
+            ft = None
             continue
         else:
-            print("You said you want {} restaurant?".format(p))
+            return ft, ft_confirm, p, l
+
+def get_price(ft, p, l):
+    while(1):
+        if p is None:
+            print("How expensive a restaurant do you want?")
+            ip = raw_input()
+            ft, p, l = process_p_input(ip, ft, p, l)
+
+        if p is None:
+            continue
+        else:
+            print("You said you want {} priced restaurant?".format(p))
             ip = raw_input()
             p_confirm = process_confirmation_ip(ip)
-            print("confirmation:", p_confirm)
         
         if p_confirm is 0:
+            p = None
             continue
+        else:   
+            return ft, p_confirm, p, l
+        
+def get_location(ft, p, l):
+    while(1):
+        if l is None:
+            print("Ok where do you want the restaurant to be located?")
+            ip = raw_input()
+            ft, p, l = process_l_input(ip, ft, p, l)
 
-    if l_confirm is False:
-        print("Ok where do you want the restaurant to be located?")
-        ip = raw_input()
-        any_found, ft, p, l = process_input(ip)
-
-        if l is None and any_found is False:
+        if l is None:
             continue
         else:
-            print("You said you want restaurant in {}?".format(l))
+            print("You said you want restaurant in {} location?".format(l))
             ip = raw_input()
             l_confirm = process_confirmation_ip(ip)
-            print("confirmation:", l_confirm)
         
         if l_confirm is 0:
+            l = None
             continue
+        else:   
+            return ft, l_confirm, p, l
+
+def show_results(ft, p, l):
+    results = []
+    print(results)
+    if ft is not 'any':
+        # print("ft is not any", ft)
+        for k in restaurants.keys():
+            # print(restaurants[k]["foodtype"])
+            if restaurants[k]["foodtype"] == ft:
+                # print("ft is matching")
+                results.append(k)
+    else:
+        for k in restaurants.keys():
+            results.append(k)
+    print(results)
+    if p is not 'any':
+        for k in results:
+            if restaurants[k]['price'] != p:
+                results.remove(k)
+    print(results)           
+    if l is not 'any':
+        for k in results:
+            if restaurants[k]['location'] != l:
+                results.remove(k)
+                
+    print(results)
+    for k in results:
+        print(k, restaurants[k])
+
+def fill_slots():
+    ft = None
+    p = None
+    l = None
+    ft, ft_confirm, p, l = get_foodtype(ft, p, l)    
+    ft, p_confirm, p, l = get_price(ft, p, l)
+    ft, l_confirm, p, l = get_location(ft, p, l)
+    return ft, ft_confirm, p, p_confirm, l, l_confirm
+
+ft, ft_confirm, p, p_confirm, l, l_confirm = fill_slots()
+print(ft, ft_confirm, p, p_confirm, l, l_confirm)
+
+show_results(ft, p, l)
+

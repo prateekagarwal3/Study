ddf6eb3f9c0237e0968150aeca0bd83bc440f8dd
prateekagarwal3 <pa59946@usc.edu>
2020-02-11 21:02:41 -0800

    basic code working quering left

--- CS644/HW1/main.py
+++ CS644/HW1/main.py
@@ -11,6 +11,7 @@ prices = ["cheap", "medium-priced", "expensive"]
 locations = ["marina del ray", "korea town", "venice", "playa vista", "santa monica", "hollywood"]
 
 def process_input(ip):
+    ip = ip.lower()
     any_found = False
     ft = None
     p = None
@@ -36,24 +37,61 @@ def process_input(ip):
     return any_found, ft, p, l
 
 def process_confirmation_ip(ip):
-    if ip.find("Yes") != -1:
+    ip = ip.lower()
+    if ip.find("yes") != -1:
         return 1
     else:
         return 0
     
-while(1):
-    print("What type of restaurant do you want?")
-    ip = raw_input()
-    any_found, ft, p, l = process_input(ip)
+ft_confirm = False
+p_confirm = False
+l_confirm = False
 
-    if ft is None and any_found is False:
-        continue
-    else:
-        print("You said you want {} restaurant?".format(ft))
+while(1):
+    if ft_confirm is False:
+        print("What type of restaurant do you want?")
         ip = raw_input()
-        ft_confirm = process_confirmation_ip(ip)
-        print(ft_confirm)
-    
-    if ft_confirm is 0:
-        continue
-    
+        any_found, ft, p, l = process_input(ip)
+
+        if ft is None and any_found is False:
+            continue
+        else:
+            print("You said you want {} restaurant?".format(ft))
+            ip = raw_input()
+            ft_confirm = process_confirmation_ip(ip)
+            print("confirmation:", ft_confirm)
+        
+        if ft_confirm is 0:
+            continue
+
+    if p_confirm is False:
+        print("How expensive a restaurant do you want?")
+        ip = raw_input()
+        any_found, ft, p, l = process_input(ip)
+
+        if p is None and any_found is False:
+            continue
+        else:
+            print("You said you want {} restaurant?".format(p))
+            ip = raw_input()
+            p_confirm = process_confirmation_ip(ip)
+            print("confirmation:", p_confirm)
+        
+        if p_confirm is 0:
+            continue
+
+    if l_confirm is False:
+        print("Ok where do you want the restaurant to be located?")
+        ip = raw_input()
+        any_found, ft, p, l = process_input(ip)
+
+        if l is None and any_found is False:
+            continue
+        else:
+            print("You said you want restaurant in {}?".format(l))
+            ip = raw_input()
+            l_confirm = process_confirmation_ip(ip)
+            print("confirmation:", l_confirm)
+        
+        if l_confirm is 0:
+            continue
